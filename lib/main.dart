7f7c0a49d02dcb36c30caed0665b78fef62e54e1
cris <c.rugelesd@uniandes.edu.co>
2025-12-06 21:29:41 -0500

    Import firebase and fix ios launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // <--- Importante
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 
 import './ProviderState.dart';
@@ -13,7 +14,9 @@ import './ScheduleTripPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Verificamos si hay usuario logueado
   final User? currentUser = FirebaseAuth.instance.currentUser;
